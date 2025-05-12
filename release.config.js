65e47d7d53085654283508c0d896636a3086e85d
Lưu Quốc Bình <34769433+LiuQBinh@users.noreply.github.com>
2025-05-12 17:55:18 +0700

    feat: thêm tính năng lọc

--- release.config.js
+++ release.config.js
@@ -1,18 +1,18 @@
 // release.config.js
 module.exports = {
-    branches: ["main"], // hoặc "master" nếu bạn dùng tên này
+    branches: ["main"],
     plugins: [
-        "@semantic-release/commit-analyzer", // Phân tích commit để xác định loại version
-        "@semantic-release/release-notes-generator", // Tạo ghi chú phát hành
-        "@semantic-release/changelog", // Cập nhật CHANGELOG.md
-        "@semantic-release/npm", // Tăng version package.json & publish lên npm
-        [
-            "@semantic-release/git",
-            {
-                assets: ["package.json", "CHANGELOG.md"],
-                message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-            },
-        ],
-        "@semantic-release/github", // Tạo GitHub release (tuỳ chọn)
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/changelog",
+      [
+        "@semantic-release/git",
+        {
+          assets: ["CHANGELOG.md", "package.json"],
+          message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        },
+      ],
+      "@semantic-release/github"
     ],
-};
+  };
+  
