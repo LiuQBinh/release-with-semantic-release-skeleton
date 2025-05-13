fab9eaa720505283c09d6836217a1ba2d25d0733
Lưu Quốc Bình <34769433+LiuQBinh@users.noreply.github.com>
2025-05-13 10:47:22 +0700

    feat: a big feature
    BREAKING CHANGE: this feature changes the core API usage

--- release.config.js
+++ release.config.js
@@ -1,23 +1,24 @@
 // release.config.js
 module.exports = {
-    branches: ["main"],
-    "releaseRules": [
-      { "type": "feat", "release": "minor" },
-      { "type": "fix", "release": "patch" },
-      { "breaking": true, "release": "major" }
-    ],
-    plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/changelog",
-      [
-        "@semantic-release/git",
-        {
-          assets: ["CHANGELOG.md", "package.json"],
-          message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-        },
-      ],
-      "@semantic-release/github"
-    ],
-  };
-  
+  branches: ["main"],
+  releaseRules: [
+    { type: "feat", release: "minor" },
+    { type: "fix", release: "patch" },
+    { breaking: true, release: "major" },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    ["@semantic-release/npm", {
+      npmPublish: false, // Set true if you want to publish to npm
+    }],
+
+    ["@semantic-release/git", {
+      assets: ["CHANGELOG.md", "package.json"],
+      message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+    }],
+    
+    "@semantic-release/github"
+  ],
+};
