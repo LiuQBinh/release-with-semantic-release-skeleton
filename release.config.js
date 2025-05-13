5ac6d358d62593dce029b71e60afcf54203053f5
Lưu Quốc Bình <34769433+LiuQBinh@users.noreply.github.com>
2025-05-13 18:03:30 +0700

    ix: SEC-123 resolve login bug

--- release.config.js
+++ release.config.js
@@ -14,10 +14,10 @@ module.exports = {
       npmPublish: false, // Set true if you want to publish to npm
     }],
 
-    // ["@semantic-release/git", {
-    //   assets: ["CHANGELOG.md", "package.json"],
-    //   message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-    // }],
+    ["@semantic-release/git", {
+      assets: ["CHANGELOG.md", "package.json"],
+      message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+    }],
     
     "@semantic-release/github"
   ],
